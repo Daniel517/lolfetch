06254e33abf7e720902a8ede59da1c89b8c53082
Daniel Ramirez <danielramirez@Daniels-MacBook-Pro-2.local>
2020-07-09 09:30:57 -0700

    Nav.js now stores entered summonername. On form submit it console logs summoner name for now but will eventually redirect to find information on player.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React , {useState} from 'react';
 import '../CSS/Nav.css';
 
 function Nav() {
+  
+  const [summonerName, setSummonerName] = useState('')
+
+  const submit = e => {
+    e.preventDefault()
+    console.log(summonerName)
+  }
+
+  const eventHandler = e => {
+    setSummonerName(e.target.value)
+  }
+
   return (
     <div className="bottom-border">
       <nav className="navbar navbar-expand-lg navbar-custom">
-        <a class="navbar-brand main-font brand" href="/">LoLFetch</a>
-        <ul class="navbar-nav ml-auto mr-5">
-          <form className="form-inline">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search Summoner Name(s):"/>
-            <button class="btn btn-success">Search</button>
+        <a className="navbar-brand main-font brand" href="/">LoLFetch</a>
+        <ul className="navbar-nav ml-auto mr-5">
+          <form className="form-inline" onSubmit={submit}>
+            <input className="form-control mr-sm-2" type="text" placeholder="Search Summoner Name(s):" onChange={eventHandler}/>
+            <button className="btn btn-success">Search</button>
           </form>
         </ul>
       </nav>
