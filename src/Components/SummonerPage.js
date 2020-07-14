04e0b1c982a65a8575317103a6dd2e70b2b7c5c7
Daniel Ramirez <danielramirez@Daniels-MacBook-Pro-2.local>
2020-07-14 10:02:53 -0700

    SummonerPage.js makes api call to get summoner account data and renders child component after successful api call.

--- src/Components/SummonerPage.js
+++ src/Components/SummonerPage.js
@@ -1,14 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../CSS/SummonerPage.css';
+import axios from 'axios'
+
+import SummonerInfo from '../Components/SummonerInfo'
 
 function SummonerPage({match}) {
 
-    const [summonerName, setSummonerName] = useState('')
+    const [summonerName, setSummonerName] = useState(match.params.summoner)
     const [summonerInfo, setSummonerInfo] = useState({})
+    const [rerender, setRerender] = useState(false)
+
+    useEffect(() => {
+        getInfo(summonerName)
+    }, [summonerName, rerender])
+
+    //Makes API call for summoner account data
+    const getInfo = async summonerName => {
+        const accInfoUrl = `http://192.168.0.15:5000/lflolapi/summonerInfo?summonerName=${summonerName}`
+        await axios
+            .get(accInfoUrl)
+            .then(res => {
+                setSummonerInfo(res.data)
+                setRerender(true)
+            })
+            .catch(err => {
+                //Display error page
+                console.log(err)
+            })
+    }
+
+    if (!rerender) return false
 
     return(
         <div>
-            
+            <SummonerInfo summonerName={summonerName} summonerId={summonerInfo.id}/>
         </div>
     );
 
