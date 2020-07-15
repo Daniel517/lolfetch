49303dbd5e51d7e725ad43bff2a673d0f8a66e0b
Daniel Ramirez <danielramirez@Daniels-MacBook-Pro-2.local>
2020-07-15 06:57:22 -0700

    Created SummonerHistory component to hold past matches data. Also will hold options to filter by queue.

---
+++ src/Components/SummonerHistory.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import '../CSS/SummonerHistory.css';
+import axios from 'axios'
+
+function SummonerInfo(props) {
+
+    const [accountId, setAccountId] = useState(props.accountId)
+    const [matchHistory, setMatchHistory] = useState([])
+    const [rerender, setRerender] = useState(false)
+
+
+    useEffect(() => {
+        if (!rerender) {
+            getMatchHistory(accountId)
+        }
+    }, [rerender])
+
+    //Makes API call for summoner ranks
+    const getMatchHistory = async summonerId => {
+        const rankUrl = `http://192.168.0.15:5000/lflolapi/matchHistory?accountId=${accountId}`
+        await axios
+            .get(rankUrl)
+            .then(res => {
+                setMatchHistory(res.data.matches)
+                setRerender(true)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    if (!rerender) return false
+
+    return(
+        <div className="card my-4 w-75 mx-auto">
+            <div className="card-header bg-header text-center">
+                <label className="title third-font">Match History</label>
+                <div className="d-flex text-center my-2 second-font options">
+                    <div className="flex-fill">
+                        Norms
+                    </div>
+                    <div className="flex-fill">
+                        Solo/Duo
+                    </div>
+                    <div className="flex-fill">
+                        Flex
+                    </div>
+                    <div className="flex-fill">
+                        Clash
+                    </div>
+                </div>
+            </div>
+            <div className="card-content">
+                <div className="d-flex">
+                    <div className="card flex-fill m-3">
+                        <div className="card-header">
+                            OK
+                        </div>
+                    </div>
+                    <div className="card flex-fill m-3">
+                        <div className="card-header">
+                            OK
+                        </div>
+                    </div>
+                    <div className="card flex-fill m-3">
+                        <div className="card-header">
+                            OK
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default SummonerInfo;
